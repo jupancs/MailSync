2d6c91a3ef8ceb063e6579605838e40c308027dd
Ab261 <avakash261@gmail.com>
2019-05-28 12:16:23 +0530

    Version 2.0 of ViewModel with some more attributes added

--- mailsync-anroid/app/src/main/java/edu/ua/cs/nrl/mailsync/EmailViewModel.java
+++ mailsync-anroid/app/src/main/java/edu/ua/cs/nrl/mailsync/EmailViewModel.java
@@ -1,11 +1,39 @@
 package edu.ua.cs.nrl.mailsync;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 
-public class EmailViewModel extends ViewModel {
+import java.util.concurrent.ScheduledExecutorService;
+
+import butterknife.Unbinder;
+import edu.ua.cs.nrl.mailsync.database.NdnDBConnection;
+import edu.ua.cs.nrl.mailsync.relayer.Relayer;
+
+public class EmailViewModel extends AndroidViewModel {
         private MutableLiveData<String> email;
         private MutableLiveData<String> password;
+        private EmailRepository emailRepository;
+        private boolean networkStatus;
+
+
+        private boolean isFirstTime = true;
+
+        public EmailViewModel(@NonNull Application application) {
+                super(application);
+                this.emailRepository=new EmailRepository(getApplication().getApplicationContext());
+        }
+
+//        public EmailViewModel(@NonNull Application application){
+//                super(application);
+//                this.emailRepository= new EmailRepository();
+//        }
 
 
 
@@ -22,4 +50,30 @@ public class EmailViewModel extends ViewModel {
                 }
                 return email ;
         }
+
+        public void setNetworkStatus(boolean networkStatus) {
+                this.networkStatus = networkStatus;
+        }
+
+        public boolean getNetworkStatus() {
+                return networkStatus;
+        }
+
+        public void startServer(){
+
+        }
+
+        public void init(){
+
+        }
+        public boolean isNetworkAvailable() {
+                ConnectivityManager connectivityManager
+                        = (ConnectivityManager) getApplication()
+                        .getApplicationContext()
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        }
+
+
 }
